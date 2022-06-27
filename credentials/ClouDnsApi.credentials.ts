6b31260268d4240921a13a1c6dd74bd9340d3b93
xlincw0w <khaled.khazem@yahoo.com>
2022-06-27 09:33:02 +0100

    Fixed linter, Test cred, changing image, password field, simplifyOutput ...

--- credentials/ClouDnsApi.credentials.ts
+++ credentials/ClouDnsApi.credentials.ts
@@ -1,27 +1,27 @@
-import { ICredentialType, INodeProperties } from "n8n-workflow";
+import { ICredentialType, INodeProperties } from 'n8n-workflow';
 
 export class ClouDnsApi implements ICredentialType {
-	name = "clouDnsApi";
-	displayName = "ClouDns API";
-	documentationUrl = "clouDns";
+	name = 'clouDnsApi';
+	displayName = 'ClouDns API';
+	documentationUrl = 'clouDns';
 	properties: INodeProperties[] = [
 		// The credentials to get from user and save encrypted.
 		// Properties can be defined exactly in the same way
 		// as node properties.
 		{
-			displayName: "Auth ID",
-			name: "authId",
-			type: "string",
-			default: "",
+			displayName: 'Auth ID',
+			name: 'authId',
+			type: 'string',
+			default: '',
 		},
 		{
-			displayName: "Auth Password",
-			name: "authPassword",
-			type: "string",
+			displayName: 'Auth Password',
+			name: 'authPassword',
+			type: 'string',
 			typeOptions: {
 				password: true,
 			},
-			default: "",
+			default: '',
 		},
 	];
 }
